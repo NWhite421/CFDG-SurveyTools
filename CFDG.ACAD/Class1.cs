6d56e477afb7b42af0a49e2006ca2cefb6521310
NWhite421 <57015867+NWhite421@users.noreply.github.com>
2021-01-26 15:09:21 -0500

    Initial push for dev branch

---
+++ CFDG.ACAD/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoCADApplication = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+using Civil3DApplication = Autodesk.Civil.ApplicationServices.CivilApplication;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace HNH.ACAD
+{
+    public class Class1
+    {
+
+    }
+}
